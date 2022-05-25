8b790dc573cee81ad49e386096d0ffaa776d72d7
Chinaza <ugwuokechinazajames@gmail.com>
2022-05-25 18:47:02 +0100

    Made a quick home page

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('API.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
