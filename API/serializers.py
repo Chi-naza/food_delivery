293a97b11adb721b73672431b24d9971425f0803
Chinaza <ugwuokechinazajames@gmail.com>
2022-06-04 14:12:03 +0100

    Debugging continued

--- API/serializers.py
+++ API/serializers.py
@@ -5,31 +5,31 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from API.models import CustomUser, Food, Products
 
 
-# customUser serializer overidding the one dj-rest-auth
-class CustomRegisterSerializer(RegisterSerializer):
-    phone_number = serializers.CharField(max_length=30)
+# # customUser serializer overidding the one dj-rest-auth
+# class CustomRegisterSerializer(RegisterSerializer):
+#     phone_number = serializers.CharField(max_length=30)
 
-    # Define transaction.atomic to rollback the save operation in case of error
-    @transaction.atomic
-    def save(self, request):
-        user = super().save(request)
-        user.phone_number = self.data.get('phone_number')
-        user.save()
-        return user
+#     # Define transaction.atomic to rollback the save operation in case of error
+#     @transaction.atomic
+#     def save(self, request):
+#         user = super().save(request)
+#         user.phone_number = self.data.get('phone_number')
+#         user.save()
+#         return user
 
 
-# custom userDetails serializer to retrieve user details & info
-class CustomUserDetailsSerializer(serializers.ModelSerializer):
+# # custom userDetails serializer to retrieve user details & info
+# class CustomUserDetailsSerializer(serializers.ModelSerializer):
     
-    class Meta:
-        model = CustomUser
-        fields = (
-            'pk',
-            'email',
-            'phone_number',
-            'username',
-        )
-        read_only_fields = ('pk', 'email', 'phone_number', 'username',)
+#     class Meta:
+#         model = CustomUser
+#         fields = (
+#             'pk',
+#             'email',
+#             'phone_number',
+#             'username',
+#         )
+#         read_only_fields = ('pk', 'email', 'phone_number', 'username',)
 
 
 class FoodSerializer(serializers.ModelSerializer):
--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('API.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
